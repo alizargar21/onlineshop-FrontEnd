0bdd68467cf460f2d3a0555c03dfb82708c2134e
Ali Zargar <mrzargar1995@gmail.com>
2022-11-01 00:22:31 +0330

    rdy to merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
-import { Route, Routes, Router ,useParams } from "react-router-dom";
-import AboutUs from "./container/Pages/Aboutus/AboutUs";
-import { ToastContainer, Slide, Zoom, Flip } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomePage from "../src/container/Pages/Home/HomePage";
 import CartPage from "./container/Pages/cart/CartPage";
@@ -14,65 +13,59 @@ import AuthProvider from "./Provider/AuthProvider";
 import CheckOut from "./container/Pages/CheckOutPage/CheckOut";
 import ProfilePage from "./container/Pages/Profile/Profile";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
-import Categories from "./container/Pages/Products&Categories/Categories";
-import ProductListContainer from "./container/Pages/products/ProductList/ProductListContainer";
 import ThemeProvider from "./Provider/ThemeProvider";
 import FavoriteProvider from "./Provider/FavoriteProvider";
 import Favorite from "./container/Pages/FavoritePage/Favorite";
-import http from "./services/httpServices";
-import { useState , useEffect } from "react";
-
+import AllOfferProducts from "./container/Pages/OfferPage/OfferPage";
+import { Provider } from "react-redux";
+import store from "./features/store";
+import ProductsAndCategories from "./container/Pages/Products&Categories/Categories";
 
 function App() {
+ 
   
-  const Fetching = async()=> {
-    const data =await http.get("/products")
-    const allProducts =data.data
-    setProducts(allProducts)
-    
-  
-  }
-  const [products , setProducts] = useState([])
-  // const [data , setData] = useState([])
-  useEffect(() => {
-    Fetching()
-  }, [setProducts]);
   return (
     <>
-      <ThemeProvider>
-        
-        <FavoriteProvider>
-        <AuthProvider>
-          <CartProvider>
-            <ToastContainer
-              closeOnClick
-              position="top-left"
-              hideProgressBar={true}
-              transition={Flip}
-              theme={"colored" }
-            />
-            <Routes>
-              <Route path="/" element={<HomePage products={products}/>} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/checkout" element={<CheckOut />} />
-              <Route path="*" element={<NotFoundPage />} />
-              <Route path="/blogs" element={<Blogs />} />
-              <Route path="/categories" element={<Categories />} />
-              <Route path="/laptops" element={<ProductListContainer />} />
-              <Route path="/mobiles" element={<ProductListContainer />} />
-              <Route path="/cases" element={<ProductListContainer />} />
-              <Route path="/myFavorite" element={<Favorite />} />
-              <Route path={`/products/:id`} element={<SingleProduct products={products}/>} />
-            </Routes>
+      <Provider store={store}>
+        <ThemeProvider>
+          <FavoriteProvider>
+            <AuthProvider>
+              <CartProvider>
+                <ToastContainer
+                  closeOnClick
+                  position="top-left"
+                  hideProgressBar={true}
+                  transition={Flip}
+                  theme={"colored"}
+                />
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/checkout" element={<CheckOut />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                  <Route path="/blogs" element={<Blogs />} />
+                  <Route
+                    path="/offers"
+                    element={<AllOfferProducts  />}
+                  />
+                  <Route path="/products/*" element={<ProductsAndCategories/>} />
+               
+           
             
-          </CartProvider>
-        </AuthProvider>
-        </FavoriteProvider>
-      
-      </ThemeProvider>
+                  <Route path="/myFavorite" element={<Favorite />} />
+                  <Route
+                    path={`/products/:id`}
+                    element={<SingleProduct  />}
+                  />
+                </Routes>
+              </CartProvider>
+            </AuthProvider>
+          </FavoriteProvider>
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }
